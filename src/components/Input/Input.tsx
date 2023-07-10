774c943f58b9895174f7b95c1499868b41ac7eaf
Khikmet Sadigzade <sadigzade.hi@mail.ru>
2023-07-11 01:38:48 +0300

    Made validation for page /login

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,37 +1,28 @@
-import { FC, useRef, useState } from "react";
-import { RegisterOptions, useFormContext } from "react-hook-form";
+import { FC, useState } from "react";
 import { HideIcon, ShowIcon } from "@ya.praktikum/react-developer-burger-ui-components";
-import { ErrorMessage } from "@hookform/error-message";
+import { Control, UseFormRegisterReturn, useController } from "react-hook-form";
 import { LoginInputs } from "../../pages/Login/Login";
 
 type InputProps = {
   type: string;
   name: keyof LoginInputs;
-  title: string;
-  validations?: RegisterOptions;
+  label: string;
+  register: UseFormRegisterReturn;
+  errorMessage?: string;
+  control: Control<LoginInputs>;
 };
 
-const Input: FC<InputProps> = ({ validations, type, name, title }) => {
-  const inputRef = useRef<HTMLInputElement>(null);
+const Input: FC<InputProps> = ({ type, name, label, register, errorMessage, control }) => {
   const [show, setShow] = useState(false);
   const [inputActive, setInputActive] = useState(false);
-  const {
-    register,
-    formState: { errors },
-  } = useFormContext<LoginInputs>();
 
-  const onClickEmailInput = () => {
-    inputRef.current?.focus();
-  };
+  const { field } = useController({
+    name,
+    control,
+  });
 
   const onShowPassword = () => {
     setShow(!show);
-
-    setTimeout(() => {
-      if (inputRef.current) {
-        inputRef.current.selectionStart = inputRef.current.selectionEnd = 10000;
-      }
-    }, 0);
   };
 
   return (
@@ -40,35 +31,31 @@ const Input: FC<InputProps> = ({ validations, type, name, title }) => {
         className={`relative flex items-center px-5 lg:px-6 lg:py-3 border-2 border-solid ease-in duration-200 cursor-text bg-[#2F2F37] rounded-[40px] w-full ${
           inputActive ? "border-[#4c4cff]" : "border-transparent"
         }`}
-        onClick={onClickEmailInput}
       >
         <label
           className={`absolute text-[#8585AD] ease-in duration-300 z-10 ${
-            inputActive ? "top-1 lg:top-2" : "top-[14px] lg:top-5"
+            inputActive || field.value ? "top-1 lg:top-2" : "top-[14px] lg:top-5"
           }`}
         >
-          {title}
+          {label}
         </label>
         <input
-          {...register(name, validations)}
-          ref={inputRef}
+          {...register}
+          value={field.value}
+          onChange={field.onChange}
           type={show ? "text" : type}
           onFocus={() => setInputActive(true)}
           onBlur={() => setInputActive(false)}
           className="h-8 lg:h-5 bg-transparent input-help border-none outline-none caret-white w-full mt-4 text-white text-[16px] focus:text-white"
         />
 
-        {/* <ErrorMessage errors={errors} name={name} render={({ message }) => <p>{message}</p>} /> */}
         {type === "password" && (
           <div className="flex items-center cursor-pointer" onClick={onShowPassword}>
             {show ? <HideIcon type="primary" /> : <ShowIcon type="primary" />}
           </div>
         )}
       </div>
-      {/* {errors[name] && (
-        <span className="text-[red]">{errors[name]?.message?.toString() || "Error!"}</span>
-      )} */}
-      {errors[name] && <p>{errors[name]?.message}</p>}
+      {errorMessage && <span className="text-[red]">{errorMessage}</span>}
     </div>
   );
 };
--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,96 +1,68 @@
-import { FormEvent, useCallback, useRef, useState } from "react";
-import { useDispatch } from "../../hooks/hooks";
-import { Link } from "react-router-dom";
-import { Button } from "@ya.praktikum/react-developer-burger-ui-components";
-import { loginRequestThunk } from "../../services/actions/login";
-import Input from "../../components/Input/Input";
-import { FormProvider, SubmitErrorHandler, SubmitHandler, useForm } from "react-hook-form";
 import { ZodType, z } from "zod";
+import { Link } from "react-router-dom";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { Button } from "@ya.praktikum/react-developer-burger-ui-components";
+import Input from "../../components/Input/Input";
 
 export type LoginInputs = {
   email: string;
+  password: string;
 };
 
+const schema: ZodType<LoginInputs> = z.object({
+  email: z.string().nonempty("Обязательное поле").email("Некорректный e-mail"),
+  password: z.string().nonempty("Обязательное поле").min(8, "Пароль должен быть больше 8 символов"),
+});
+
 export const LoginPage = () => {
-  const dispatch = useDispatch();
-  const onSubmit: SubmitHandler<LoginInputs> = (data) => console.log(data);
-  const onError: SubmitErrorHandler<LoginInputs> = (error) => console.log(error);
-
-  const schema: ZodType<LoginInputs> = z.object({
-    email: z
-      .string({
-        required_error: "Обязательно для заполнения",
-      })
-      .email({ message: "Некорректный e-mail" })
-      .default(""),
-  });
-
-  const methods = useForm<z.infer<typeof schema>>({
+  const {
+    handleSubmit,
+    register,
+    control,
+    formState: { errors },
+  } = useForm<z.infer<typeof schema>>({
     resolver: zodResolver(schema),
     defaultValues: {
       email: "",
+      password: "",
     },
   });
 
-  // useCallback();
-  // (e: FormEvent<HTMLFormElement>) => {
-  //   e.preventDefault();
-  //   // dispatch(
-  //   //   loginRequestThunk({
-  //   //     email: email.value,
-  //   //     password: password.value,
-  //   //   }),
-  //   // );
-  // },
-  // [dispatch],
+  const formSubmit: SubmitHandler<LoginInputs> = (data) => console.log(data);
 
   return (
     <section className="mt-4 md:mt-[180px] px-2">
-      <FormProvider {...methods}>
-        <form
-          className="flex flex-col items-center"
-          onSubmit={methods.handleSubmit(onSubmit, onError)}
-        >
-          <h1 className="mb-6 text-[28px] text-center">Вход</h1>
-          <div className="flex flex-col gap-y-5 lg:gap-y-6 max-w-[480px] w-full">
-            <Input
-              type={"email"}
-              name={"email"}
-              title={"E-mail"}
-              validations={{
-                required: true,
-                pattern: {
-                  value:
-                    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-                  message: "Введите корректный E-mail",
-                },
-              }}
-            />
-            {/* <Input
-              type={"password"}
-              name={"password"}
-              title={"Пароль"}
-              validations={{
-                required: true,
-                minLength: {
-                  value: 8,
-                  message: "Пароль должен быть больше 8 символов",
-                },
-              }}
-            /> */}
-            <Button
-              id={"button-login"}
-              htmlType="submit"
-              type="primary"
-              size={window.innerWidth < 768 ? "small" : "medium"}
-              extraClass="self-center"
-            >
-              Войти
-            </Button>
-          </div>
-        </form>
-      </FormProvider>
+      <form className="flex flex-col items-center" onSubmit={handleSubmit(formSubmit)}>
+        <h1 className="mb-6 text-[28px] text-center">Вход</h1>
+        <div className="flex flex-col gap-y-5 lg:gap-y-6 max-w-[480px] w-full">
+          <Input
+            type={"email"}
+            name={"email"}
+            label={"E-mail"}
+            register={{ ...register("email") }}
+            errorMessage={errors.email?.message}
+            control={control}
+          />
+          <Input
+            type={"password"}
+            name={"password"}
+            label={"Пароль"}
+            register={{ ...register("password") }}
+            errorMessage={errors.password?.message}
+            control={control}
+          />
+          <Button
+            id={"button-login"}
+            htmlType="submit"
+            type="primary"
+            size={window.innerWidth < 768 ? "small" : "medium"}
+            extraClass="self-center"
+          >
+            Войти
+          </Button>
+        </div>
+      </form>
       <div className="flex flex-col items-center gap-y-5 mt-10 lg:mt-20">
         <div className="flex flex-col lg:flex-row lg:gap-x-2 items-center">
           <span className="text_color_inactive">Вы — новый пользователь?</span>
